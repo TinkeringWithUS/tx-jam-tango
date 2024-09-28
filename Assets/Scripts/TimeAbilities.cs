925a36486a7e9df6e485c088e093d2d03baaa526
TinkeringWithUS <dannyzheng@utexas.edu>
2024-09-28 14:43:53 -0500

    got time freeze slider

--- Assets/Scripts/TimeAbilities.cs
+++ Assets/Scripts/TimeAbilities.cs
@@ -1,21 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeAbilities : MonoBehaviour
 {
     // Start is called before the first frame update
+    public KeyCode freezeKey;
+
     public GameObject[] snapshots;
     Vector3 ScaleSnapShot;
+
+
+    public Slider freezeLeftSlider;
+    public float freezeAmountLeftSeconds;
+    private float maxFreezeAmountSeconds;
+    private float freezeStart;
+    private bool isFreezed = false;
+    
     void Start()
     {
 
+        freezeLeftSlider.value = freezeLeftSlider.maxValue;
+        maxFreezeAmountSeconds = freezeAmountLeftSeconds;
     }
     
     // Update is called once per frame
     void Update()
     {
+        bool shouldFreeze = false;
+
+        if(Input.GetKey(freezeKey)) {
+            shouldFreeze = true;
+        }
+
+        TimeFreeze(shouldFreeze);
+
         TimeSnapshot();
+    }
+
+    public void TimeFreeze(bool shouldFreeze) {
+        TimeFreezable[] freezables = FindObjectsOfType<TimeFreezable>(); 
+
+        foreach (TimeFreezable freezableObj in freezables) {
+
+            if (shouldFreeze && freezeAmountLeftSeconds > 0) {
+                freezableObj.TimeFreeze();
+
+
+                isFreezed = true;
+            } else {
+                freezableObj.TimeUnfreeze();
+                isFreezed = false;
+            }
+        }
+
+
+        if (shouldFreeze) {
+            freezeAmountLeftSeconds -= Time.deltaTime;
+        }
+
+        freezeLeftSlider.value = (freezeAmountLeftSeconds / maxFreezeAmountSeconds) * freezeLeftSlider.maxValue;
     }
 
     public void TimeSnapshot()
---
+++ Assets/Scripts/TimeFreezable.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeFreezable : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    Vector2 beforeFreezeVelocity;
+
+    bool isFrozen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TimeFreeze() {
+        if(!isFrozen) {
+            beforeFreezeVelocity = rb.velocity;
+
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+
+            isFrozen = true;
+        }
+    }
+
+    public void TimeUnfreeze() {
+        if (isFrozen) {
+            rb.constraints = RigidbodyConstraints2D.None;
+
+            rb.velocity = beforeFreezeVelocity;
+
+            isFrozen = false;
+        }
+    }
+}
